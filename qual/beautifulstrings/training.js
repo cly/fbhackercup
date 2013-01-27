2ab10a3cd6d65116294e959770462a506cd5ce7b
Charlie Liang Yuan <charlie@betable.com>
2013-01-26 22:29:04 -0800

    filter half choices

--- qual/beautifulstrings/training.js
+++ qual/beautifulstrings/training.js
@@ -1,3 +1,5 @@
+var _ = require('underscore')
+
 var data = [{
     s: 'ABbCcc'
   , x: 1
@@ -30,6 +32,14 @@ var data = [{
 var letters_re = /[a-z]/
 var all_letters = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
+// Reduce brute-force set size.
+var possible_values = all_letters.map(function(letter) {
+    return {
+        letter: letter
+      , possible: _.range(1, 26 + 1)
+    }
+})
+
 for (var i = 0, ii = data.length; i < ii; ++i) {
     var line = data[i]
       , s = line.s.toLowerCase()
@@ -52,23 +62,50 @@ for (var i = 0, ii = data.length; i < ii; ++i) {
         })
     })
 
+    count_arr = count_arr.filter(function(v) {
+        return v.count > 0
+    })
+
     // Sort it by most frequently occurring letter first
     var sorted_count_arr = count_arr.sort(function(l, r) {
         return r.count - l.count
     })
 
+    sorted_count_arr.forEach(function(letter, i) {
+        var other_maximum_sum = 0
+        var max_num = 26
+
+        // Find max sum of other numbers
+        sorted_count_arr.forEach(function(inner_letter, inner_i) {
+            if (letter.letter !== inner_letter.letter) {
+                other_maximum_sum += inner_letter.count * max_num
+                max_num = max_num - 1
+            }
+        })
+
+        // Filter impossible cases!
+        var num_gte = (line.y - other_maximum_sum)/letter.count
+        possible_values.forEach(function(possible_letter) {
+            if (possible_letter.letter === letter.letter) {
+                possible_letter.possible = possible_letter.possible.filter(function(v) {
+                    return v >= num_gte
+                })
+            }
+        })
+    })
+
+/*
     var max_beauty = 26
     sorted_count_arr = sorted_count_arr.map(function(v) {
-        v.max_beauty = max_beauty
+        //v.max_beauty = max_beauty
         max_beauty = max_beauty - 1
         return v
     })
-
-
-
-
-
-
-    console.log(JSON.stringify(sorted_count_arr))
+*/
+    //console.log(JSON.stringify(sorted_count_arr))
 }
+
+console.log(JSON.stringify(possible_values))
+
+
 
