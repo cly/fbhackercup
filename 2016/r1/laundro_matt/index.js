6fa43de6b273ddd9f6de6e4c9b302c348fc0a651
Charlie Liang Yuan <charlie.liang.yuan@gmail.com>
2016-01-16 15:54:34 -0800

    implement min heap

--- 2016/r1/laundro_matt/index.js
+++ 2016/r1/laundro_matt/index.js
@@ -13,6 +13,74 @@ var outputFileName = inputFileName + '.output.txt';
 var inputFileData = fs.readFileSync(path.join(__dirname, inputFileName), {encoding: 'utf8'})
 var outputFileData = ''
 
+// Heap implementation.
+// Assume node has a cost attribute.
+var BinaryMinHeap = function() {
+    this.data = []
+}
+
+BinaryMinHeap.prototype.insert = function(value) {
+    this.data.push(value)
+    this.upHeapify(this.data.length - 1)
+}
+
+BinaryMinHeap.prototype.getRoot = function() {
+    return this.data[0]
+}
+
+BinaryMinHeap.prototype.updateRoot = function(value) {
+    if (this.data.length) {
+        this.data[0] = value
+        this.downHeapify(0)
+    }
+}
+
+BinaryMinHeap.prototype.tradeNodes = function(index, index2) {
+    var tempNode = this.data[index]
+    this.data[index] = this.data[index2]
+    this.data[index2] = tempNode
+}
+
+BinaryMinHeap.prototype.downHeapify = function(index) {
+    // [0, 1, 2, 3, 4, 5, 6]
+    var leftChildIndex = index * 2 + 1
+    var rightChildIndex = index * 2 + 2
+    var current = this.data[index]
+
+    if (rightChildIndex < this.data.length) {
+        // Both children exists.
+        var leftChild = this.data[leftChildIndex]
+        var rightChild = this.data[rightChildIndex]
+
+        if (rightChild < leftChild && rightChild < current) {
+            this.tradeNodes(index, rightChildIndex)
+            this.downHeapify(rightChildIndex)
+        } else if (leftChild < current) {
+            // leftChild is smaller or equal to rightChild.
+            this.tradeNodes(index, leftChildIndex)
+            this.downHeapify(leftChildIndex)
+        }
+    } else if (leftChildIndex < this.data.length) {
+        // Only one child.
+        var leftChild = this.data[leftChildIndex]
+        if (leftChild < current) {
+            this.tradeNodes(index, leftChildIndex)
+            this.downHeapify(leftChildIndex)
+        }
+    }
+}
+
+BinaryMinHeap.prototype.upHeapify = function(index) {
+    // [0, 1, 2, 3, 4, 5, 6]
+    if (index > 0) {
+        var parentIndex = Math.floor((index - 1) / 2)
+        if (this.data[index] < this.data[parentIndex]) {
+            this.tradeNodes(index, parentIndex)
+            this.upHeapify(parentIndex)
+        }
+    }
+}
+
 // Parse and compute.
 var lines = inputFileData.split('\n')
 var T = parseInt(lines.shift(), 10)
@@ -28,7 +96,9 @@ while(t < T) {
     var D = args[3] // Dryer takes D minutes
     var W = lines.shift().split(' ').map(v => parseInt(v, 10))
 
-    console.log(args, W)
+    var h = new BinaryMinHeap();
+
+    console.log(h)
 
     // outputFileData += 'Case #' + t + ': ' + sum + '\n'
 }
