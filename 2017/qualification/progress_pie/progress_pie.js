a391b3243b9cd147fb553815ba46fc60214be436
Charlie Liang Yuan <charlie.liang.yuan@gmail.com>
2017-01-08 14:56:41 -0800

    done

--- 2017/qualification/progress_pie/progress_pie.js
+++ 2017/qualification/progress_pie/progress_pie.js
@@ -3,27 +3,43 @@ const process = require('process')
 
 const input = fs.readFileSync(process.argv[2], 'utf8')
 const rows = input.trim().split('\n').map(_ => _.trim())
-const T = +rows.shift() // Number of points.
-const points = rows.map(_ => {
+const countTests = +rows.shift() // Number of tests.
+const tests = rows.map(_ => {
     const line = _
     const columns = _.split(' ')
     return {
-        P: +columns[0],
-        X: +columns[1],
-        Y: +columns[2]
+        angle: +columns[0] * 0.01 * 2 * Math.PI,
+        point: [+columns[1], +columns[2]]
     }
 })
-const validPoints = points.slice(0, T)
+const validTests = tests.slice(0, countTests)
 
-// If X, Y is outside circle, then return case white.
-// Else, find line extending from middle to X, Y.
-// Check if this line is within the bounding line.
-const cases = validPoints.map((point, i) => {
-    return `Case #${i + 1}`
+const CENTER = [50, 50]
+const RADIUS = 50
+const getDistanceFromCenter = point => {
+    return Math.sqrt(
+        Math.pow(point[1] - CENTER[1], 2) +
+        Math.pow(point[0] - CENTER[0], 2)
+    )
+}
+const getAngle = point => {
+    return Math.atan2(point[0] - CENTER[0], point[1] - CENTER[1])
+}
+
+const cases = validTests.map((test, i) => {
+    if (getDistanceFromCenter(test.point) > RADIUS) {
+        // Point is outside circle, return white.
+        return `Case #${i + 1}: white`
+    } else {
+        // See if angle of point is smaller than angle of percentage.
+        if (getAngle(test.point) <= test.angle) {
+            return `Case #${i + 1}: black`
+        } else {
+            return `Case #${i + 1}: white`
+        }
+    }
 })
 
-console.log(T)
-console.log(validPoints)
-console.log(cases)
 const output = cases.join('\n')
+console.log(output)
 fs.writeFileSync(process.argv[3], output, 'utf8')
